5103d316296033980481ab46091b53d1d6a21c5c
Vladyslav Bulavchuk <wvsac37@gmail.com>
2024-12-01 18:37:35 +0200

    The code for the unit test is ready.

--- app/src/test/java/com/example/wordcountapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/wordcountapp/ExampleUnitTest.java
@@ -10,8 +10,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private final TextCount textCount = new TextCount();
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void textCountWordsNullInput() {
+        assertEquals(0, textCount.countWords(null));
+    }
+    @Test
+    public void testCountCharactersNullInput() {
+        assertEquals(0, textCount.countCharacters(null));
+    }
+
+    @Test
+    public void testCountWordsValidInput() {
+        assertEquals(5, textCount.countWords("I am a test string!"));
+    }
+    @Test
+    public void testCountCharactersValidInput() {
+        assertEquals(22, textCount.countCharacters("This is a test string."));
+    }
+
+    @Test
+    public void testCountWordsEmptyInput() {
+        assertEquals(0, textCount.countWords(""));
+    }
+    @Test
+    public void testCountCharactersEmptyInput() {
+        assertEquals(0, textCount.countCharacters(""));
     }
 }
